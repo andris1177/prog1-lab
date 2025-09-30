f29ab98d625988321ce86825f3200e1a1d92c436
andris1177 <laszloandris1@gmail.com>
2025-09-30 11:52:04 +0200

    add 3 task solution

--- lab3/diofantoszi.c
+++ lab3/diofantoszi.c
@@ -2,8 +2,22 @@
 
 int main()
 {
-    int a, b, c, x, y;
+    int a, b, c;
 
     scanf("%d%d%d", &a, &b, &c);
     
+    for (int i = 0; a*i <= c ; i++)
+    {
+        for (int j = 0; b*j <= c; j++)
+        {
+            if (i*a + j*b == c)
+            {
+                if (i == 0 || j == 0)
+                    continue;
+                printf("%d %d\n", i, j);
+            }
+        }
+    }
+
+    return 0;
 }
