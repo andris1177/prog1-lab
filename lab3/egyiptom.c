aa9c050e0a6491836aa2a1ca2d2ef2770b2f4fe1
andris1177 <laszloandris1@gmail.com>
2025-09-30 12:12:47 +0200

    add task solution

--- lab3/egyiptom.c
+++ lab3/egyiptom.c
@@ -2,12 +2,21 @@
 
 int main()
 {
-    int m, n, x;
+    int x, y, a = 1;
 
-    scanf("%d%d", &m, &n);
-    
-    while  (m > 0)
+    scanf("%d%d", &x, &y); 
+
+    while (x != 0)
     {
-        
+        if (x*a >= y)
+        {
+            x = x*a-y;
+            y = y*a;
+            printf("%d ", a);
+        }
+
+        a++;
     }
+
+    return 0;
 }
